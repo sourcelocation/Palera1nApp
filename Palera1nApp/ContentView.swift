740198adb3e3c2f3cbe64be292ea064ba2b2dbde
Matthew <anisovich.matthew@gmail.com>
2022-11-08 19:39:10 +0000

    some code I don't understand and taken from stack overflow

--- Palera1nApp/ContentView.swift
+++ Palera1nApp/ContentView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var logsText: String = ""
+    
+    init() {
+        UITextView.appearance().backgroundColor = .clear
+    }
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.init(red: <#T##Double#>, green: <#T##Double#>, blue: <#T##Double#>), .black]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [.init(hex: "071B33"), .init(hex: "833F46"), .init(hex: "FFB123")]), startPoint: .topTrailing, endPoint: .bottomLeading)
                 .ignoresSafeArea()
             VStack {
-                Image("palera1n-white")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
+                VStack {
+                    HStack {
+                        Image("palera1n-white")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80)
+                        Text("Palera1n")
+                            .font(.system(size: 48, weight: .bold))
+                    }
+                    .padding(8)
+                    Text("by nebula, mineek, nathan")
+                        .font(.body)
+                    
+                    TextEditor(text: $logsText)
+                        .background(Color.clear)
+                        .colorMultiply(.red)
+                }
             }
+            .foregroundColor(.white)
             .padding()
         }
     }
